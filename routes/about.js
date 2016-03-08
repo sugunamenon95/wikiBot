ac1e6819d709dc9a5d80c9df14066609ad1dfac8
Suguna Menon <trainee.suguna@corp.avinashi.com>
2016-03-08 14:43:13 +0530

    Initial commit

---
+++ routes/about.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Suguna on 22-01-2016.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET about page. */
+router.get('/', function(req, res, next) {
+    res.render('about', {title: 'About'});
+});
+
+module.exports = router;
